94afcb0d3689242256973ba5e545264644e705a9
Lans12345 <olanalans12345@gmail.com>
2024-04-08 12:34:58 +0800

    initial project setup

---
+++ lib/utlis/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+var primary = const Color(0xff0A98D5);
+
+var secondary = const Color(0xff43C3FA);
+var black = const Color(0xff151515);
+var grey = const Color(0xffB3B3B3);
