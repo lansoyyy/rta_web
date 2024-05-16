704cd252bc517462448f82bbaa832479ddf34f74
Lans12345 <olanalans12345@gmail.com>
2024-05-16 14:26:22 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:rta_web/screens/auth/login_screen.dart';
 import 'package:rta_web/screens/home_screen.dart';
@@ -6,7 +7,16 @@ import 'package:rta_web/screens/tabs/alltickets_tab.dart';
 import 'package:rta_web/screens/tabs/enforcers_tab.dart';
 import 'package:rta_web/screens/tabs/my_profile_tab.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyDkJByZydja4BEzJ_KKMnR4sUtGqkwuwM8",
+          authDomain: "thesis-rta.firebaseapp.com",
+          projectId: "thesis-rta",
+          storageBucket: "thesis-rta.appspot.com",
+          messagingSenderId: "635736180770",
+          appId: "1:635736180770:web:9e8edf266d459b22cd227b"));
   runApp(const MyApp());
 }
 
